9777d51239f4e07149cbeb6249abc02cc5d04fc4
EvgenyLeukhiin <eugenel@effective.band>
2020-06-18 22:10:39 +0600

    Update

--- src/js/common.js
+++ src/js/common.js
@@ -8,4 +8,19 @@ $(document).ready(() => {
 
     burgerIcon.toggleClass('open');
   });
+
+
+
+  // click to more btn
+  const moreBtn = $('.js-more-btn');
+
+  moreBtn.on('click', e => {
+    e.preventDefault();
+
+    // сохраняем расстояние до h1 в переменную
+    let scrollToPromoBlock = $('.promo-block').offset().top;
+
+    // скролим тело документа (html и body для кроссбраузерности) на расстояние scrollHight 500млс
+    $('html, body').animate({ scrollTop: scrollToPromoBlock }, 500);
+  });
 });
