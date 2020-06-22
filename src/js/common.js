d8bfb8b61f03efd2d2f8b7b3bb2379e69f23eb6d
EvgenyLeukhiin <eugenel@effective.band>
2020-06-22 23:54:49 +0600

    Add video-modals

--- src/js/common.js
+++ src/js/common.js
@@ -1,3 +1,8 @@
+// import izimodal
+import iziModal from 'izimodal/js/iziModal';
+$.fn.iziModal = iziModal;
+
+
 $(document).ready(() => {
 
   // toggle header menu
@@ -7,5 +12,43 @@ $(document).ready(() => {
     e.preventDefault();
 
     burgerIcon.toggleClass('open');
+  });
+
+  // open modals
+  const openModalLink = $('.open-modal-click');
+
+  openModalLink.on('click', function(e) {
+    e.preventDefault();
+
+    // clicked link data
+    const openModalLinkData = $(this).data();
+
+    // modal element
+    const modal = $('#modal-youtube');
+
+    // to reset initial iframeURL !!!
+    modal.iziModal('destroy');
+
+    modal.iziModal({
+      title: openModalLinkData.izimodalTitle,
+      subtitle: openModalLinkData.izimodalSubtitle,
+      headerColor: 'black',
+      background: 'black',
+      borderBottom: false,
+      closeButton: true,
+      transitionIn: 'fadeInDown',
+      iframe: true,
+      iframeHeight: 315,
+      width: 560,
+      fullscreen: true,
+      iframeURL: openModalLinkData.izimodalIframeurl,
+      history: false,
+      loop: false,
+
+      // to reset fullscreen state
+      onClosing: function () {
+        modal.iziModal('setFullscreen', false);
+      },
+    });
   });
 });
